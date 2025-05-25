cc7615d79576c4c2085c367e6783b2700410a884
Piyush Jha <piyushjha@Piyushs-MacBook-Air.local>
2025-05-25 19:27:00 +0530

    feat: basic differentiation

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Arrays;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        Calculator c = new Calculator();
-        double result1 = c.calculate(5,"sqrt");
-        double result2 = c.calculate(4,5,"+");
-        double[] result3 = c.calculate("2x² + 5x - 3");
 
-        System.out.println(Arrays.toString(result3));
+
     }
 }
